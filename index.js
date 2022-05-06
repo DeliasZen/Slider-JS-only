06b121022524a7c27f3da3761002c30914be9cb7
Ilya Shutikov <DeliasZen@gmail.com>
2022-05-06 21:56:09 +0300

    fix: directory changed

--- index.js
+++
@@ -1,9 +0,0 @@
-import { createElement, createMarkup, createStyle } from './dom.js';
-import Carousel from './carousel.js';
-createMarkup()
-createStyle()
-
-
-const carousel = new Carousel(true, 2000)
-
-carousel.init()
